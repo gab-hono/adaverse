6c31d9dc8a52705e010a710cdbd2035bdd43df41
Gabriel H. <gabrielhono.o@gmail.com>
2026-03-31 12:20:35 +0200

    Création de function qui crée un path github pour récuperer img thumbnail

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,3 +7,17 @@ export function generateSlug(titre: string): string {
     .trim()
     .replace(/\s+/g, "-");
 }
+
+//esta función toma el user y repo a través del link de github, y crea el path para recuerar la imagen thumbnail
+export function getThumbnailUrl(lienGithub: string): string {
+  try {
+    const url = new URL(lienGithub);
+    const parts = url.pathname.split("/").filter(Boolean);
+    const user = parts[0];
+    const repo = parts[1];
+
+    return `https://github.com/${user}/${repo}/blob/main/thumbnail.png?raw=true`;
+  } catch {
+    return "/no-image-available.jpg"; // imagen por defecto (en carpeta "public") si el link no resulta ser válido
+  }
+}
